8519d7b77e2fa347b6a8c6612505af7199a93084
Alex Sun <alexsun.srand.nums@gmail.com>
2025-12-28 10:37:04 -0600

    Update page title in layout.tsx
    We here fr

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Next.js on GitHub Pages',
+  title: 'Next.js on GitHub Pages_we here fr',
   description: 'Deploy your static Next.js site to GitHub Pages.',
 };
 
